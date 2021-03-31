de1a7957af64292be08f2cabef8d526130fe2300
ArtCenter1 <artceter1@gmail.com>
2021-04-01 07:43:36 +0800

    re-arange

--- Frontend/Main.js
+++ Frontend/Main.js
@@ -1,2 +1,4 @@
-Moralis.initialize("irF5RmoJtDU4e23DsV0QYLyMBoSwBJC6UHwRc1zR");
+Moralis.initialize("irF5RmoJtDU4e23DsV0QYLyMBoSwBJC6UHwRc1zR", "YOUR_JAVASCRIPT_KEY");
+//javascriptKey is required only if you have it on server.
+
 Moralis.serverURL = 'https://rowkubhlshxr.moralis.io:2053/server'
