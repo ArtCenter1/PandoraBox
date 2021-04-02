1b5d65090a0e7650d9f826c463e29b1fd9161aeb
ArtCenter1 <artceter1@gmail.com>
2021-04-02 22:25:50 +0800

    catch up

--- Frontend/Main.js
+++ Frontend/Main.js
@@ -1,3 +1,26 @@
 Moralis.initialize("irF5RmoJtDU4e23DsV0QYLyMBoSwBJC6UHwRc1zR");
 //
 Moralis.serverURL = 'https://rowkubhlshxr.moralis.io:2053/server'
+
+init = async () => {
+    window.web3 = await Moralis.web3.enable();
+    initUer();
+}
+
+initUser = async() => {
+    if (await Moralis.User.current()){
+        hideElement(userConnectButton);
+        showElement(usrProfileButton);
+    }else{
+        showElement(userConnectButton);
+        hideElement(userProfileButton);
+    }
+}
+
+login = async () => {
+  try{
+    await Moralis.webs.autoenticate();
+  } catch (error){
+
+  }
+}
