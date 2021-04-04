247fafd0c35383b6fa76effc9738a2b0df9ab6e5
ArtCenter1 <artceter1@gmail.com>
2021-04-04 23:41:16 +0800

    metaMask local server login test

--- Frontend/Main.js
+++ Frontend/Main.js
@@ -1,14 +1,14 @@
 Moralis.initialize("irF5RmoJtDU4e23DsV0QYLyMBoSwBJC6UHwRc1zR");
 Moralis.serverURL = 'https://rowkubhlshxr.moralis.io:2053/server'
-const TOKEN_CONTRACT_ADDRESS = "0xa13f135eed8B734DE667339cB73E033F34428132";
+//const TOKEN_CONTRACT_ADDRESS = "0xa13f135eed8B734DE667339cB73E033F34428132";
 
 //enable Moralis web3 service
 init = async () => {
-    hideElement(userItemsSection);
-    hideElement(userInfo);
-    hideElement(createItemForm);
+    //hideElement(userItemsSection);
+    //hideElement(userInfo);
+    //hideElement(createItemForm);
     window.web3 = await Moralis.Web3.enable();
-    window.tokenContract = new web3.eth.Contract(tokenContractAbi, TOKEN_CONTRACT_ADDRESS);
+    //window.tokenContract = new web3.eth.Contract(tokenContractAbi, TOKEN_CONTRACT_ADDRESS);
     initUser();
 }
 
@@ -16,21 +16,28 @@ initUser = async() => {
     if (await Moralis.User.current()){
       hideElement(userConnectButton);
       showElement(userProfileButton);
-      showElement(openCreateItemButton);
-      showElement(openUserItemsButton);
-      loadUserItems();
+      //showElement(openCreateItemButton);
+      //showElement(openUserItemsButton);
+      //loadUserItems();
     }else{
       showElement(userConnectButton);
       hideElement(userProfileButton);
-      hideElement(openCreateItemButton);
-      hideElement(openUserItemsButton);
+      //hideElement(openCreateItemButton);
+      //hideElement(openUserItemsButton);
     }
 }
 
 login = async () => {
   try{
     await Moralis.webs.autoenticate();
+    initUser();
   } catch (error){
-
+      alert(error)
   }
 }
+
+
+hideElement = (element) => element.style.display ="none";
+showElement = (element) => element.style.display ="block";
+
+const userConnectButton = document.getElementById("btnConnect");
