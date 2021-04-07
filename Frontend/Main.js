7c1cf67253a819ac92aa6d93e1f29234fa22add1
ArtCenter1 <artceter1@gmail.com>
2021-04-07 21:45:37 +0800

    add user Avatar Img

--- Frontend/Main.js
+++ Frontend/Main.js
@@ -37,19 +37,51 @@ login = async () => {
   }
 }
 
+logout = async () => {
+  await Moralis.User.logOut();
+  hideElement(userInfo);
+  initUser();
+}
+
 openUserInfo = async () => {
   user = await Moralis.User.current();
   if (user){    
+      const email = user.get('email');
+      if(email){
+          userEmailField.value = email;
+      }else{
+          userEmailField.value = "";
+      }
+
+      userUsernameField.value = user.get('username');
+
+      const userAvatar = user.get('avatar');
+      if(userAvatar){
+          userAvatarImg.src = userAvatar.url();
+          showElement(userAvatarImg);
+      }else{
+          hideElement(userAvatarImg);
+      }
+
       showElement(userInfo);
   }else{
       login();
   }
 }
 
-logout = async () => {
-  await Moralis.User.logOut();
-  hideElement(userInfo);
-  initUser();
+saveUserInfo = async () => {
+  user.set('email', userEmailField.value);
+  user.set('username', userUsernameField.value);
+  //still need to handle duplicate username from users here
+
+  if (userAvatarFile.files.length > 0) {
+      const avatar = new Moralis.File("avatar.jpg", userAvatarFile.files[0]);
+      user.set('avatar', avatar);
+  }
+
+  await user.save();
+  alert("User info saved successfully!");
+  openUserInfo();
 }
 
 
@@ -65,7 +97,13 @@ userProfileButton.onclick = openUserInfo;
 
 //  User profile
 const userInfo = document.getElementById("userInfo");
+const userUsernameField = document.getElementById("txtUsername");
+const userEmailField = document.getElementById("txtEmail");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
 document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
 document.getElementById("btnLogout").onclick = logout;
+document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
 
 init();
