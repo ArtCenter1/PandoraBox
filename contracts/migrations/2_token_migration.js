fb4273dd3ad9ae6f4b17d494654305a06b9d679a
ArtCenter1 <artceter1@gmail.com>
2021-04-10 22:15:58 +0800

    fix migration

--- contracts/migrations/2_token_migration.js
+++ contracts/migrations/2_token_migration.js
@@ -1,4 +1,4 @@
-const PandoraBoxToken = artifacts.require("PandoraBoxToken");
+const PandoraBoxToken = artifacts.require("PandoraBoxToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(PandoraBoxToken);
